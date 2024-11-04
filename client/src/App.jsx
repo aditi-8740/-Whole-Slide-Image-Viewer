86f21e744583b737dfd477626571e4b8034774fa
Aditi Sharma <aditisharma3711.as@gmail.com>
2024-11-04 18:24:26 +0530

    Tailwindcss setup & registration page code in server & client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,49 @@
+import axios from 'axios'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+axios.defaults.baseURL = "https://whole-slide-image-viewer-server.vercel.app/"
+// axios.defaults.baseURL = "http://localhost:8000"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  async function registerUser(event) {   //send request to api
+      event.preventDefault();           //prevents form from submitting ,Not reloading page.
+      await axios.post('/register',{    //data we want to send
+          name,
+          email,
+          password,
+      })
+      .then((res)=>{
+          console.log(res)    //resolved value of await axios post request
+          console.log(res.data)
+      }
+          
+      )
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="text-center grow flex flex-col justify-center h-screen">
+        <h2 className="text-2xl ">Register</h2>
+        <form className="max-w-lg mx-auto" onSubmit={registerUser}>
+            <input type="text" placeholder="fullname"
+                value={name}
+                onChange={(event)=>{ setName(event.target.value) }} 
+            />
+            <input type="email" placeholder="youremail@gmail.com"
+                value={email}
+                onChange={event => setEmail(event.target.value)}
+            />
+            <input type="password" placeholder="password"
+                value={password}
+                onChange={event => setPassword(event.target.value)}
+            />
+            <button className="button-primary">Register</button>
+        </form>
+    </div>
+)
 }
 
 export default App
--- server/index.js
+++ server/index.js
@@ -1,8 +1,38 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const mongoose= require('mongoose')
+const USER = require('./models/User')
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors({
+  credentials: true,  //allowing cookies to be sent
+  origin: 'https://whole-slide-image-viewer-client.vercel.app',    //ORIGIN tells what app can communicate with API
+  // origin: 'http://localhost:5173',    //ORIGIN tells what app can communicate with API
+}))
+
+mongoose
+  .connect("mongodb+srv://aditisharma3711as:7JXDCn5mhckcNJmz@cluster0.uwg59.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log("MongoDB connection error: ", err));
 
 app.get('/', function (req, res) {
   res.send('Hello World')
+})
+
+app.post('/register', async (req,res)=>{
+    const {name, email, password} = req.body;
+
+    // register the user
+    const createdUser = await USER.create({
+        name: name,
+        email: email,
+        password: password
+    })
+
+    res.status(201).json(createdUser);
 })
 
 app.listen(8000 , ()=> console.log("server started on 8000"))
